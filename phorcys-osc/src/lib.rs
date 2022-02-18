a4f111431f8c664a27201cc27f5f711a97251d65
Yū Kobayashi <kb10uy@kb10uy.org>
2022-02-18 18:31:49 +0900

    Only receive literal

--- phorcys-osc/src/lib.rs
+++ phorcys-osc/src/lib.rs
@@ -8,10 +8,10 @@ pub mod packet;
 /// Constructs directly `Packet`.
 #[macro_export]
 macro_rules! osc_packet {
-    ($addr:expr) => {
+    ($addr:literal) => {
         PacketBuilder::new($addr).map(|b| b.build())
     };
-    ($addr:expr => { $($args:expr),* $(,)? }) => {
+    ($addr:literal { $($args:expr),* $(,)? }) => {
         PacketBuilder::new($addr).map(|mut b| {
             let arguments = vec![
                 $($args.into()),*,
@@ -20,5 +20,4 @@ macro_rules! osc_packet {
             b.build()
         })
     };
-
 }
